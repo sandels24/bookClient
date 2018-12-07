8c2698f815e84077978d98a1ae7a162cf1fc221a
sandels24 <saiteja.sandela@amwater.com>
2018-12-07 15:01:30 -0500

    Added Read functionality

--- src/app/main/form/form.component.ts
+++ src/app/main/form/form.component.ts
@@ -1,26 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import {Services} from '../../services/app.services'
+import {Book} from './book';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
+  providers : [Services]
 })
 export class FormComponent implements OnInit {
 
   authorname;
   bookname;
   publisheddate;
-  constructor() { }
+  created;
+  books = [];
+  show = false;
+
+  constructor(private service : Services) { }
 
   ngOnInit() {
   }
 
-  myClickFunction(event) { 
-
-    alert("Button is clicked");
+  myClickFunction(event) {
+    this.show = false;
     console.log(this.authorname);
     console.log(this.bookname);
     console.log(this.publisheddate);
-    
+    this.service.addBook(this.bookname,this.authorname,this.publisheddate).subscribe(data => this.created="Book Created");
+ }
+
+ showAllBooks(){
+
+  this.service.getbooks().subscribe(data => { 
+    this.books = [];
+    data.forEach(element => {
+      this.books.push(element);
+    });
+    this.show = true;
+    console.log(this.books);
+  });
  }
 }
--- src/app/services/app.services.ts
+++ src/app/services/app.services.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import {Book} from '../main/form/book';
 
 
 @Injectable()
 export class Services {
-    configUrl = "localhost:8888/books"
+    configUrl = "http://localhost:8888/books"
     constructor(private http: HttpClient) {
-
-     }
-    addBook(name,author) {
-        return this.http.get(this.configUrl+"/"+name+"/"+author);
-      }
+    }
+    addBook(name,author,publisheddate) {
+        return this.http.get(this.configUrl+"/"+name+"/"+author+"/"+publisheddate);
+    }
+    getbooks() {
+        return this.http.get<Book[]>(this.configUrl+"/");
+    }
 }
